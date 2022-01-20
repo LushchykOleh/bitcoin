b325a329caf233a78d36aaec99882b0b71f3892c
LushchykOleh <94828705+LushchykOleh@users.noreply.github.com>
2022-01-20 15:46:01 +0200

    responsiv

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,9 @@
 window.$ = window.jQuery = require('jquery');
 
-
-
-//-----swiper
-$(document).ready(function () {
-  /* Инициализация swiper, когда документ загрузился */  
-  var mySwiper = new Swiper ('.swiper-container', {
-    /* Параметры */
-   
-    loop: true
-  })  
-  
-  window.onload = function(){
-
-    var swiper = new Swiper('.swiper-container', {
-        pagination: {
-            el: '.swiper-pagination',
-            dynamicBullets: true,
-        },
-        });
-}
-});
-//---end swiper
+//Swiper-1-header
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+Swiper.use([ Autoplay, Navigation, Pagination ]);
+//---import marquee from 'jquery.marquee';
 
 $(document).ready(function (){
 
@@ -31,31 +13,38 @@ $('.burger-btn').on('click', function() {
     //$('body').toggleClass('no-scroll');
 })
 
+//search
+// let search = document.querySelector(".search")
+// let btn = document.querySelector(".search-button")
+// let input = document.querySelector(".input")
+// btn.addEventListener('click',()=>{
+//         search.classList.toggle('active');
+//         input.focus()
+//
+
 
 //swiper
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    //direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      //draggable: true
-    },
-  });
+ // About slider
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 5000,
+        },
+        slidesPerView: 1,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.header-slider .swiper-button-next',
+            prevEl: '.header-slider .swiper-button-prev',
+        },      
+    });
 
+ //Tabs
+    $('.tabs__button').on('click', function () {
+        $(".tabs .tabs__button").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tabs__item").hide().eq($(this).index()).fadeIn();
+    }).eq(0).addClass("active");
+    $(".tabs__item").eq(0).fadeIn();
+})
 
 //Anchor links
 $('a').on('click', function (e) {
@@ -74,4 +63,4 @@ $('a').on('click', function (e) {
 
 
 
-})
+
