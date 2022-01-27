59a3f2b866a6ab331065dbaf9535923adad205ae
LushchykOleh <94828705+LushchykOleh@users.noreply.github.com>
2022-01-27 13:53:12 +0200

    added new block

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,34 @@
 window.$ = window.jQuery = require('jquery');
 
 //Swiper-1-header
-import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
-Swiper.use([ Autoplay, Navigation, Pagination ]);
+import Swiper, {
+    Autoplay,
+    Navigation,
+    Pagination
+} from 'swiper';
+Swiper.use([Autoplay, Navigation, Pagination]);
 //---import marquee from 'jquery.marquee';
 
-$(document).ready(function (){
+$(document).ready(function () {
 
-//Mobile menu
-$('.burger-btn').on('click', function() {
-    $('.menu-icon').toggleClass('opened');
-    //$('body').toggleClass('no-scroll');
-})
+    //Mobile menu
+    $('.burger-btn').on('click', function () {
+        $('.menu-icon').toggleClass('opened');
+        //$('body').toggleClass('no-scroll');
+    })
 
-//search
-// let search = document.querySelector(".search")
-// let btn = document.querySelector(".search-button")
-// let input = document.querySelector(".input")
-// btn.addEventListener('click',()=>{
-//         search.classList.toggle('active');
-//         input.focus()
-//
+    //search
+    // let search = document.querySelector(".search")
+    // let btn = document.querySelector(".search-button")
+    // let input = document.querySelector(".input")
+    // btn.addEventListener('click',()=>{
+    //         search.classList.toggle('active');
+    //         input.focus()
+    //
 
 
-//swiper
- // About slider
+    //swiper
+    // About slider
     const swiper = new Swiper('.swiper-container', {
         loop: true,
         autoplay: {
@@ -35,10 +39,60 @@ $('.burger-btn').on('click', function() {
         navigation: {
             nextEl: '.header-slider .swiper-button-next',
             prevEl: '.header-slider .swiper-button-prev',
-        },      
+        },
     });
 
- //Tabs
+    // resti slider
+    //Client slider
+    const client_swiper = new Swiper('.client_swiper', {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+        },
+        slidesPerView: 1,
+        spaceBetween: 30,
+        // autoHeight: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+
+    });
+
+    //Video-button
+    const videoPlayer = $('.video video');
+
+    function playPause() {
+        if (videoPlayer.paused)
+            videoPlayer.play();
+        else
+            videoPlayer.pause();
+    }
+    //EndVideoButton
+
+    // const videoPlayer = document.getElementById("video1");
+
+    // function playPause() {
+    //     if (video.paused)
+    //         video.play();
+    //     else
+    //         Video.pause();
+    // }
+
+    //--
+    $('.video__play').on('click', function () {
+        const videoPlayer = $('#video');
+        $(this).toggleClass('paused');
+        const video = videoPlayer.get(0);
+        if (video.paused)
+            video.play();
+        else
+            video.pause();
+    })
+    //--
+
+    //Tabs
     $('.tabs__button').on('click', function () {
         $(".tabs .tabs__button").removeClass("active").eq($(this).index()).addClass("active");
         $(".tabs__item").hide().eq($(this).index()).fadeIn();
@@ -49,18 +103,13 @@ $('.burger-btn').on('click', function() {
 //Anchor links
 $('a').on('click', function (e) {
     e.preventDefault();
-     const hh = $('.header').outerHeight();
-     if(this.hash !== '') {
-         const hash = this.hash;
-         $('html, body').animate({
-             scrollTop: $(hash).offset().top - hh
-         }, 300, function () {
-             window.location.hash = hash - hh;
-         })
-     }
- })
-
-
-
-
-
+    const hh = $('.header').outerHeight();
+    if (this.hash !== '') {
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - hh
+        }, 300, function () {
+            window.location.hash = hash - hh;
+        })
+    }
+})
